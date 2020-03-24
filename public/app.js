6f6a383437b38b7eb4ed3617b5dd27cf63994925
Felipe César <felipecesr@gmail.com>
2020-03-24 19:13:08 -0300

    add more stores

---
+++ public/app.js
@@ -0,0 +1,91 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    items: [
+      {
+        id: 1,
+        name: 'Luiz Pastel',
+        phone: '85999525379',
+        whatsapp: '5585999525379'
+      },
+      {
+        id: 2,
+        name: 'Louro Jucá',
+        phone: '85996414697'
+      },
+      {
+        id: 3,
+        name: 'Mercadinho Gomes',
+        phone: '85999051584'
+      },
+      {
+        id: 4,
+        name: 'Águia da Serra',
+        phone: '85996877186'
+      },
+      {
+        id: 5,
+        name: 'Romyla Veras',
+        phone: '85996708082'
+      },
+      {
+        id: 6,
+        name: 'Pizzaria O Neném',
+        phone: '85997631711',
+        whatsapp: '5585996647772'
+      },
+      {
+        id: 7,
+        name: 'Top Lanches',
+        phone: '8599658-8278'
+      },
+      {
+        id: 8,
+        name: 'Glauciane Marmitaria',
+        phone: '85997443637'
+      },
+      {
+        id: 9,
+        name: 'N-Burguer',
+        phone: '85997621482'
+      },
+      {
+        id: 10,
+        name: 'C5 Lanches',
+        phone: '85996594054'
+      },
+      {
+        id: 11,
+        name: 'Comercial Bom Frango',
+        phone: '85991512676',
+        whatsapp: '5585999490964'
+      },
+      {
+        id: 12,
+        name: 'Mercantil Bem-te-vi',
+        phone: '85999006021'
+      },
+      {
+        id: 13,
+        name: 'Cantina do Pastor',
+        phone: '85996590379'
+      },
+      {
+        id: 14,
+        name: 'Jucy Salgados',
+        phone: '85997876032'
+      },
+      {
+        id: 15,
+        name: 'Wilsinho e Gilmara',
+        phone: '85999382082'
+      },
+      {
+        id: 16,
+        name: 'Mercantil Aki tá bom',
+        phone: '85999747859'
+      }
+    ]
+  }
+});
