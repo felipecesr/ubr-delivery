80cd437cc1cb92d24f83c5e14029dd063fd3376a
Felipe César <felipecesr@gmail.com>
2020-03-26 22:22:11 -0300

    update html

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,14 @@ import { Category } from '../components/Category';
 import { categories } from '../api/categories';
 
 const Home = () => (
-  <div className="container">
-    <main>
-      <h1 className="title">Uruburetama Delivery</h1>
-      <div className="container">
-        {categories.map(category => (
-          <Category key={category.name} category={category} />
-        ))}
-      </div>
-    </main>
-  </div>
+  <main className="container">
+    <h1 className="title">Uruburetama Delivery</h1>
+    <div className="container">
+      {categories.map(category => (
+        <Category key={category.name} category={category} />
+      ))}
+    </div>
+  </main>
 );
 
 export default Home;
