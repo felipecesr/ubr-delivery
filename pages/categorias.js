5539f33cb1cffa645e50df97e9079ef1df966d52
Felipe César <felipecesr@gmail.com>
2020-03-28 10:29:43 -0300

    add grid

---
+++ pages/categorias.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Categories } from '../templates/categories';
+
+const Categorias = () => <Categories />;
+
+export default Categorias;
---
+++ templates/categories/Grid/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 1fr;
+  grid-gap: 0.5em;
+
+  > * {
+    background-color: darkgray;
+    color: white;
+    padding: 2em;
+    border-radius: 0.5em;
+  }
+`;
