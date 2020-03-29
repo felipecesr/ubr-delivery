94232cac884d8f27a70fd9ee66258f89413b40d2
Felipe César <jeanfelipecc@gmail.com>
2020-03-28 23:32:32 -0300

    fix GTM

--- components/ScriptTags/TagManager.js
+++ components/ScriptTags/TagManager.js
@@ -5,13 +5,11 @@ const TagManagerHead = () => (
   <script
     dangerouslySetInnerHTML={{
       __html: `
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER}');</script>
-        <!-- End Google Tag Manager -->
+        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER}');
       `
     }}
   />
@@ -19,16 +17,14 @@ const TagManagerHead = () => (
 
 // Além disso, cole esse código imediatamente após a tag de abertura <body>:
 const TagManagerBody = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER}"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-      `
-    }}
-  />
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER}"
+      height="0"
+      width="0"
+      style="display:none;visibility:hidden"
+    ></iframe>
+  </noscript>
 );
 
 export { TagManagerHead, TagManagerBody };
