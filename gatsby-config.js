350a158664cfc513638c5d6f5fff14293456df39
Felipe César <felipecesr@gmail.com>
2020-04-13 21:48:04 -0300

    seo improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@felipecesr`,
+    siteUrl: `https://ubr-delivery.netlify.com`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -45,6 +46,7 @@ module.exports = {
         defaultDataLayer: { platform: "gatsby" },
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
