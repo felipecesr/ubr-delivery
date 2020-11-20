5eb754b608f8286c8e0de0af1f026c7bc5aafb94
Felipe César <felipecesr@gmail.com>
2020-11-20 11:15:13 -0300

    chore: use file system route api

--- src/pages/{UbrDeliveryCategory.slug}.js
+++ src/pages/{UbrDeliveryCategory.slug}.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "@components/layout"
 import SEO from "@components/seo"
 import { ContainerCategory } from "@components/Container/styles"
 import Nav from "@components/Nav"
-import * as S from "./styled"
+import Card from "@components/Card"
+import { spacing } from "@styles/tokens"
+
+export const CardWrapper = styled(Card)`
+  margin-top: ${spacing.md};
+
+  &:first-child {
+    margin-top: 0;
+  }
+`
 
 const Category = ({ data }) => {
-  const [category] = data.allUbrDeliveryCategory.edges
+  const [category] = data.allUbrDeliveryCategory.nodes
 
   return (
     <Layout>
-      <SEO title={category.node.name} lang="pt-br" />
-      <Nav name={category.node.name} />
+      <SEO title={category.name} lang="pt-br" />
+      <Nav name={category.name} />
       <ContainerCategory as="section">
-        {category.node.commerces.map(item => (
-          <S.CardWrapper
+        {category.commerces.map(item => (
+          <CardWrapper
             key={item.name}
             name={item.name}
             phone={item.phone}
@@ -32,18 +42,16 @@ const Category = ({ data }) => {
 export const query = graphql`
   query Category($slug: String!) {
     allUbrDeliveryCategory(filter: { slug: { eq: $slug } }) {
-      edges {
-        node {
+      nodes {
+        name
+        slug
+        commerces {
           name
-          slug
-          commerces {
-            name
-            phone
-            whatsapp
-            openingHours {
-              close
-              open
-            }
+          phone
+          whatsapp
+          openingHours {
+            close
+            open
           }
         }
       }
